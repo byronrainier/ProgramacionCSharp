3a3b4ac0dae45b0249ce6d6e5c42031577c545a2
Byron Rainier Henriquez Diaz <56271561+byronrainier@users.noreply.github.com>
2021-01-29 19:27:08 -0400

    Crecion de las entidades Curso y Departamento.

---
+++ Clase/CourseManagment.Domain/Entidades/Curso.cs
@@ -0,0 +1,41 @@
+﻿namespace CourseManagment.Domain.Entidades
+{
+    public class Curso
+    {
+        #region "Propiedades"
+
+        private int cursoID;
+
+        public int CursoID
+        {
+            get { return cursoID; }
+            set { cursoID = value; }
+        }
+
+        private string titulo;
+
+        public string Titulo
+        {
+            get { return titulo; }
+            set { titulo = value; }
+        }
+
+        private int credito;
+
+        public int Credito
+        {
+            get { return credito; }
+            set { credito = value; }
+        }
+
+        private int departamentoID;
+
+        public int DepartamentoID
+        {
+            get { return departamentoID; }
+            set { departamentoID = value; }
+        }
+
+        #endregion
+    }
+}
---
+++ Clase/CourseManagment.Domain/Entidades/Departamento.cs
@@ -0,0 +1,35 @@
+﻿namespace CourseManagment.Domain.Entidades
+{
+   public  class Departamento
+    {
+        #region "Propiedades"
+
+        
+
+        private int departamentoID;
+
+        public int DepartamentoID
+        {
+            get { return departamentoID; }
+            set { departamentoID = value; }
+        }
+
+        private string nombreDep;
+
+        public string NombreDepartamento
+        {
+            get { return nombreDep; }
+            set { nombreDep = value; }
+        }
+
+        private int presupuesto;
+
+        public int Presupuesto
+        {
+            get { return presupuesto; }
+            set { presupuesto = value; }
+        }
+
+        #endregion
+    }
+}
